e6b85906cf7ddcb0800aa1dc885af305363619e3
Volker Hartmann <volker.hartmann@kit.edu>
2024-11-22 16:09:59 +0100

    Update Docu to create both Versions V1 and V2.

--- makeRestDocu.sh
+++ makeRestDocu.sh
@@ -4,3 +4,23 @@ asciidoc -b docbook restDocu.adoc
 pandoc -f docbook -t markdown_strict restDocu.xml -o restDocu.md
 #sed -i "s/^>\s*\(.*$\)/\1/" restDocu.md
 sed -i "s/\*\([^\*]\+\)\*\([^\*]\)/'\1'\2/g" restDocu.md
+
+asciidoc -b docbook restDocuV2.adoc
+pandoc -f docbook -t markdown_strict restDocuV2.xml -o restDocuV2.md
+#sed -i "s/^>\s*\(.*$\)/\1/" restDocu.md
+sed -i "s/\*\([^\*]\+\)\*\([^\*]\)/'\1'\2/g" restDocuV2.md
+echo "In beiden xml-Dateien noch "
+echo "    linenumbering=\"options=\"unwrap\"\""
+echo "und "
+echo "    language=\"options=\"nowrap\"\""
+echo "durch"
+echo "    linenumbering=\"unnumbered\" options=\"unwrap\"\""
+echo "    language=\"json\" options=\"nowrap\""
+echo "ersetzen! "
+echo "....................."
+echo "Dann "
+echo "    sed -i \"s/\*\([^\*]\+\)\*\([^\*]\)/'\1'\2/g\" restDocu.md"
+echo "und "
+echo "    sed -i \"s/\*\([^\*]\+\)\*\([^\*]\)/'\1'\2/g\" restDocuV2.md"
+echo "ausführen."
+
